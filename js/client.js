5647f8b441fe50fcbe063df65a249f226853edb7
Dominik Meduna <medunadominik@gmail.com>
2023-06-26 22:56:30 +0200

    -rewrote to use array and client class -organized the js code into js folder

---
+++ js/client.js
@@ -0,0 +1,8 @@
+export class Client {
+    constructor(firstName, surname, age, mobileNumber ){
+        this.firstName = firstName
+        this.surname = surname
+        this.age = age
+        this.mobileNumber = mobileNumber
+    }
+}
